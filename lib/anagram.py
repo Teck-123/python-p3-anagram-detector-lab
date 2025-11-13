fe55b4b450c83c9504c926a86f620b06dff4ffe1
Thecla Owano <teckowano@gmail.com>
2025-11-13 23:16:03 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, word_list):
+        matches = []
+        for candidate in word_list:
+            if (sorted(candidate.lower()) == self.sorted_word and 
+                candidate.lower() != self.word):
+                matches.append(candidate)
+        return matches
